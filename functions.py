5ddbd952f127f587f48bfc5ef7a14f41e031e395
johnnyj04 <johnny.jin@efrei.net>
2023-11-26 20:25:41 +0100

    second commit

--- functions.py
+++ functions.py
@@ -11,129 +11,88 @@ def list_of_files(directory, extension):
 
 
 def extraction():
+    d=['1','2','3','4','5','6','7','8','9']
     nom_f = list_of_files("./speeches",".txt")
     for i in range(len(nom_f)):
         nom_f[i] = nom_f[i][11:-4]
         for j in range(len(nom_f[i])):
-            if nom_f[i][j] == '1' or nom_f[i][j] == '2' :
+            if nom_f[i][j] in d:
                 nom_f[i] = nom_f[i][:-1]
     return(nom_f)
 
-#Association des prénoms au présidents
+
+def doublon():
+    d = extraction()
+    s=[]
+    for mot in d :
+        if mot not in s :
+            s.append(mot)
+    return s
+
 dico_presidents = {
     'Chirac': 'Jaques',
     'Giscard dEstaing': 'Valérie',
     'Hollande': 'François',
     'Macron': 'Emmanuel',
     'Mitterand': 'François',
-    'Sarkozy': 'Nicolas',
-}
+    'Sarkozy': 'Nicolas',}
 
-nom = ['Chirac','Giscard dEstaing','Hollande','Macron','Mitterrand','Sarkozy']
-
-#Affichage des noms des présidents
-def p_names ():
-   p_names = list_of_files("./speeches", ".txt")
-   list_names = []
-   for file in p_names :
-       name = file[11 : len(file)-4]
-       while ord(name[len(name)-1]) >= 48 and ord(name[len(name)-1]) <= 57 :
-           name = name[:len(name)-1]
-       if name not in list_names :
-           list_names.append(name)
-   return list_names
-
-import os
-
-def minuscule_accent():
-
-    # Chemin du dossier contenant les fichiers textes
-    dossier_entree = r'C:\Users\johnn\PycharmProjects\pythonProject2\venv\speeches'
-
-    # Chemin du dossier de sortie pour les fichiers en minuscules
-    dossier_sortie = r'C:\Users\johnn\PycharmProjects\pythonProject2\venv\Cleaned'
-
-    # Parcours de tous les fichiers dans le dossier d'entrée
-    for nom_fichier in os.listdir(dossier_entree):
-        chemin_fichier_entree = os.path.join(dossier_entree, nom_fichier)
-
-        # Lecture du contenu et transformation en minuscules avec les codes ASCII
-        with open(chemin_fichier_entree, 'r',) as fichier_entree:
-            contenu_minuscules = ''
-            for char in fichier_entree.read():
-                if 65 <= ord(char) <= 90:
-                    contenu_minuscules += chr(ord(char) + 32)
-                else:
-                    if char in {'é', 'è', 'ê', 'à', 'â', 'î', 'ô', 'û', 'ë', 'ï'}:
-                        contenu_minuscules += {'é': 'e', 'è': 'e', 'ê': 'e', 'à': 'a', 'â': 'a', 'î': 'i', 'ô': 'o', 'û': 'u', 'ë': 'e', 'ï': 'i'}[char]
+def conv_en_minuscule():
+    speeches = r'C:\Users\johnn\PycharmProjects\pythonProject2\venv\speeches'
+    cleaned = r'C:\Users\johnn\PycharmProjects\pythonProject2\venv\Cleaned'
+    a={".", ",", ";", "!", "?", "(", ")", ":", "/"}
+    for nom_f in os.listdir(speeches):
+        f_entree = os.path.join(speeches, nom_f)
+        with open(f_entree, 'r',encoding='utf-8' ) as f:
+            m = ''
+            for c in f.read():
+                 if 65 <= ord(c) <= 90:
+                     m += chr(ord(c) + 32)
+                 else:
+                    if c in {'é', 'è', 'ê', 'à', 'â', 'î', 'ô', 'û', 'ë', 'ï','ù','ç'}:
+                         m += {'é': 'e', 'è': 'e', 'ê': 'e', 'à': 'a', 'â': 'a', 'î': 'i', 'ô': 'o', 'û': 'u','ù':'u' ,'ë': 'e','ï': 'i','ç':'c'}[c]
+                    elif c in {"'","-"}:
+                        m += " "
+                    elif c in a:
+                        m += ""
                     else:
-                        contenu_minuscules += char
-
-            # Construction du chemin du fichier de sortie
-            chemin_fichier_sortie = os.path.join(dossier_sortie, nom_fichier)
-
-            # Écriture du contenu modifié dans le fichier de sortie
-            with open(chemin_fichier_sortie, 'w',) as fichier_sortie:
-                fichier_sortie.write(contenu_minuscules)
+                         m += c
+            f_sortie = os.path.join(cleaned, nom_f)
+            with open(f_sortie, 'w',encoding='utf-8' ) as f1:
+                 f1.write(m)
 
 
 
 
-def sans_ponctuation():
-    # Chemin du dossier contenant les fichiers en minuscules
-    dossier_entree = r'C:\Users\johnn\PycharmProjects\pythonProject2\venv\Cleaned'
 
-    # Liste des caractères de ponctuation à exclure
-    caracteres_ponctuation = set(".,;!?()[]{}<>:;-_+=*&^%$#@!`~\"'/\\|")
-
-    # Parcours de tous les fichiers dans le dossier d'entrée
-    for nom_fichier in os.listdir(dossier_entree):
-        chemin_fichier_entree = os.path.join(dossier_entree, nom_fichier)
-
-        # Vérification si le chemin correspond à un fichier texte
-        if os.path.isfile(chemin_fichier_entree) and nom_fichier.endswith('.txt'):
-            # Lecture du contenu et suppression des caractères de ponctuation
-            with open(chemin_fichier_entree, 'r') as fichier_entree:
-                contenu = fichier_entree.read()
-                contenu_sans_ponctuation = ''.join(char if char not in caracteres_ponctuation else '' for char in contenu)
-
-            # Écriture du contenu modifié dans le même fichier
-            with open(chemin_fichier_entree, 'w') as fichier_sortie:
-                fichier_sortie.write(contenu_sans_ponctuation)
-
-def nombre_de_mot(dossier):
+def tf(dossier):
     resultats = {}
     d = 0
-
-    # Obtenez la liste des fichiers dans le dossier
     fichiers = [f for f in os.listdir(dossier) if os.path.isfile(os.path.join(dossier, f))]
-
     for fichier in fichiers:
-        chemin_fichier = os.path.join(dossier, fichier)
-
-        with open(chemin_fichier, 'r') as f:
+        cf = os.path.join(dossier, fichier)
+        with open(cf, 'r') as f:
             contenu = f.read()
             chaines = contenu.split(" ")
-
-            # Créez un sous-dictionnaire pour chaque fichier
-            sous_dictionnaire = {}
             for mot in chaines:
-                if mot != " ":
-                    sous_dictionnaire[d] = mot
-                    d += 1
-
-            # Ajoutez l e sous-dictionnaire au dictionnaire principal
-            resultats[fichier] = sous_dictionnaire
+                if "\n" in mot:
+                    mot=mot[:-1]
+                if mot in resultats:
+                    resultats[mot]+=1
+                else:
+                    resultats[mot] = 1
 
     return resultats
 
 
-
-
-
 def calculer_idf(repertoire_corpus):
+    # Initialiser un dictionnaire pour stocker le nombre de documents contenant chaque mot
     documents_contenant_mot = {}
+
+    # Initialiser le compteur total de documents dans le corpus
     total_documents = 0
+
+    # Parcourir chaque fichier dans le répertoire du corpus
     for nom_fichier in os.listdir(repertoire_corpus):
         chemin_fichier = os.path.join(repertoire_corpus, nom_fichier)
 
@@ -156,4 +115,7 @@ def calculer_idf(repertoire_corpus):
 
     # Retourner le dictionnaire des scores IDF
     return idf_scores
+
+
+    contenu_modifie = contenu_sans_ponctuation.replace("'", ' ').replace('-', ' ')
 
--- main.py
+++ main.py
@@ -1,21 +1,10 @@
-# Call of the function
 from functions import*
 
-
-directory = "./speeches"
-files_names = list_of_files(directory, "txt")
-print(files_names)
-d=extraction()
-print(d)
-directory = r'C:\Users\johnn\PycharmProjects\pythonProject2\venv\speeches'
-files_names = list_of_files(directory, "txt")
-minuscule_accent()
-
-sans_ponctuation()
-
-resultat_final = nombre_de_mot(r'C:\Users\johnn\PycharmProjects\pythonProject2\venv\Cleaned')
-print(resultat_final)
-
+s=doublon()
+print(s)
+conv_en_minuscule()
+occurence = tf(r'C:\Users\johnn\PycharmProjects\pythonProject2\venv\Cleaned')
+print(occurence)
 
 repertoire_corpus_test = r'C:\Users\johnn\PycharmProjects\pythonProject2\venv\Cleaned'
 resultat_idf = calculer_idf(repertoire_corpus_test)
